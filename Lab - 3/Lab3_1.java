8ad8ac90fe720b100fd16a51fe54c4677bbc9b1b
kakadiayaprince <156084541+kakadiayaprince@users.noreply.github.com>
2024-07-09 07:54:46 +0530

    Add files via upload

---
+++ Lab - 3/Lab3_1.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class Lab3_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a size of an array : ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.println("Enter an Element : ");
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        sc.close();
+    }
+}
