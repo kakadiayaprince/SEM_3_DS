35ae9d964cd510bc7c575fe705645480a33e6d88
kakadiayaprince <156084541+kakadiayaprince@users.noreply.github.com>
2024-07-16 09:28:01 +0530

    LinkedList.java

--- Linkedlist.java
+++ Linkedlist.java
@@ -1,50 +1,72 @@
+public class LinkedList {
+    class Node {
+        int info;
+        Node link;
 
- class Node {
-    int data;
-    Node next;
+        public Node(int info) {
+            this.info = info;
+            this.link = null;
+        }
 
-    public Node(int data) {
-        this.data = data;
-        this.next = null;
-    }
+        public Node first;
 
-    
-    public Node first;
-    
-
-    public void AddInsertFirst(int data) {
-        Node newNode = new Node(data);
-        if (first == null) {
+        public void InsertAtFirst(int data) {
+            Node newNode = new Node(data);
+            if (first == null) {
+                first = newNode;
+                return;
+            }
+            newNode.link = first;
             first = newNode;
-            return;
         }
-        newNode.next = first;
-        first = newNode;
+
+        public void InsertAtOrder(int data) {
+            Node newNode = new Node(data);
+            if (first == null || newNode.info <= first.info) {
+                newNode.link = first;
+                first = newNode;
+                return;
+            }
+            Node save = null;
+            while (save.link != null && newNode.info >= save.link.info) {
+                save = save.link;
+            }
+            newNode.link = save.link;
+            save.link = newNode;
+        }
+
+        public void InsertAtLast(int data) {
+            Node newNode = new Node(data);
+
+            if (first == null) {
+                first = newNode;
+                return;
+            }
+            Node last = first;
+            while (last.link != null) {
+                last = last.link;
+            }
+            last.link = newNode;
+        }
+
+        public void display() {
+            Node current = first;
+            if (first == null) {
+                System.out.println("your Linkedlist is null");
+            }
+            while (current != null) {
+                System.out.print(current.info + ",   ");
+                current = current.link;
+            }
+        }
     }
-
-    public void display() {
-        Node curr = first;
-        if (first == null) {
-            System.out.println("your Lnkedlist is null");
-        }
-        while (curr != null) {
-            System.out.print(curr.data + ",   ");
-            curr = curr.next;
-        }
-    }
-
-}
-
-public class Linkedlist {
 
     public static void main(String[] args) {
-         Node list = new Node(0);
-        list.AddInsertFirst(5);
-        list.AddInsertFirst(8);
-        // list.AddInsertFirst(6);
-        // list.AddInsertFirst(9);
 
-        dhhdhd
+        Node list = new Node(10);
+        list.InsertAtFirst(10);
+        list.InsertAtLast(20);
+        list.InsertAtOrder(15);
         list.display();
     }
 }
